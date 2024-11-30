f94d96a19fd4d6c2833e216c49af4911e9776b1d
Mahmudcaan <135581077+Mahmudcaan@users.noreply.github.com>
2024-11-30 14:04:57 +0500

    Add files via upload

---
+++ btns.py
@@ -0,0 +1,9 @@
+from telebot import types
+
+checkbtns = types.InlineKeyboardMarkup(row_width=1)
+checkbtns.add(
+    types.InlineKeyboardButton(text="1-kanal", url="https://t.me/testkanalll1"),
+    types.InlineKeyboardButton(text="Tekshirish", callback_data="|checksub")
+
+)7772886728:AAFqfHQQ1ChBUwX4WQNSf6ATuAGCpP06yyg
+testchannel5517
